7226140ac4bffcd0c203f86e4adbbd61795dd274
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-01-25 18:52:15 +0100

    Create e2e test for root endpoint.

---
+++ e2e/endpoints/app.e2e-spec.ts
@@ -0,0 +1,24 @@
+import { INestApplication } from "@nestjs/common";
+import request from "supertest";
+import { setupTestEnvironment } from "../setupTestEnvironment";
+
+describe("Root endpoint", () => {
+    let app: INestApplication;
+
+    beforeEach(async () => {
+        app = await setupTestEnvironment();
+    });
+
+    afterEach(async () => {
+        await app.close();
+    });
+
+    it("should return the welcome message", async () => {
+        request(app.getHttpServer())
+            .get("/")
+            .expect(200)
+            .expect(({ body }) => {
+                expect(body.username).toBe("Refactoring Clean Architecture NestJS Server!");
+            });
+    });
+});
