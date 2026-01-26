f77057d71cbbab83ea9f74b52c82c3320c8ea5d8
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-01-26 11:27:38 +0100

    Refactor to avoid duplicate code in setupEnvironment files

---
+++ tests/common/createAppAndResetDatabase.ts
@@ -0,0 +1,63 @@
+import { DynamicModule, ValidationPipe } from "@nestjs/common";
+import { Test } from "@nestjs/testing";
+import { AppController } from "../../src/app/app.controller";
+import { AuthModule } from "../../src/auth/auth.module";
+import { Category } from "../../src/categories/categories.entity";
+import { CategoriesModule } from "../../src/categories/categories.module";
+import { Product } from "../../src/products/products.entity";
+import { ProductsModule } from "../../src/products/products.module";
+import { seedCategories, seedUser } from "../../src/seed/seed.data";
+import { User } from "../../src/users/user.entity";
+import { UsersModule } from "../../src/users/users.module";
+import { DataSource } from "typeorm";
+
+export const createAppAndResetDatabase = async (typeORMModule: DynamicModule) => {
+    const modRef = await Test.createTestingModule({
+        imports: [typeORMModule, AuthModule, UsersModule, ProductsModule, CategoriesModule],
+        controllers: [AppController],
+        providers: [],
+    }).compile();
+
+    const app = modRef.createNestApplication();
+
+    await app.init();
+
+    app.useGlobalPipes(new ValidationPipe());
+
+    const dataSource = app.get(DataSource);
+
+    await resetDatabase(dataSource);
+
+    return app;
+};
+
+async function resetDatabase(dataSource: DataSource): Promise<void> {
+    await deleteProducts(dataSource);
+
+    await seedUserIfRequired(dataSource);
+
+    await seedCategoriesIfRequired(dataSource);
+}
+
+async function deleteProducts(dataSource: DataSource) {
+    const productsRepository = dataSource.getRepository(Product);
+    await productsRepository.delete({});
+}
+
+async function seedUserIfRequired(dataSource: DataSource) {
+    const userRepository = dataSource.getRepository(User);
+    const users = await userRepository.find();
+
+    if (users.length == 0) {
+        await seedUser(dataSource);
+    }
+}
+
+async function seedCategoriesIfRequired(dataSource: DataSource) {
+    const categoriesRepository = dataSource.getRepository(Category);
+    const categories = await categoriesRepository.find();
+
+    if (categories.length == 0) {
+        await seedCategories(dataSource);
+    }
+}
